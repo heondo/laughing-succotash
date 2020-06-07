5a44f538dbcfb2dadce88b9c8534eae0895b8765
Heondo Kim <heondokim@gmail.com>
2020-06-06 18:44:50 -0700

    adding menu inputs to toggle the sidebar

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input, ElementRef } from '@angular/core'
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() snav: ElementRef
   constructor() {}
 
   ngOnInit() {
